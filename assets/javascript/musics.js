873a32c97bcb1a1560247b6c2df7c9ec33558c7c
Adliano <adliano@me.com>
2019-03-15 00:52:54 -0700

    move musc object to another js file

---
+++ assets/javascript/musics.js
@@ -0,0 +1,102 @@
+var musics = {
+    0: {
+        bandName: "Cece Peniston",
+        musicName: "Finaly",
+        musicID: "86037362"
+    },
+    1: {
+        bandName: "Corona",
+        musicName: "The Rhythm of the Night",
+        musicID: "316397090"
+    },
+    2: {
+        bandName: "Deee Lite",
+        musicName: "Groove Is In The Heart",
+        musicID: "214692725"
+    },
+    3: {
+        bandName: "Real McCoy",
+        musicName: "Another Night",
+        musicID: "253506396"
+    },
+    4: {
+        bandName: "Whitney Houston",
+        musicName: "It's not right, but it's okay",
+        musicID: "288857770"
+    },
+    5: {
+        bandName: "La Bouche",
+        musicName: "Be My Lover",
+        musicID: "253516188"
+    },
+    6: {
+        bandName: "Amber",
+        musicName: "This is Your Night",
+        musicID: "254564353"
+    },
+    7: {
+        bandName: "No Mercy",
+        musicName: "Where Do You Go",
+        musicID: "252889798"
+    },
+    8: {
+        bandName: "Everything but the Girl",
+        musicName: "Missing",
+        musicID: "74143904"
+    },
+    9: {
+        bandName: "Haddaway",
+        musicName: "What Is Love",
+        musicID: "253391097"
+    },
+    10: {
+        bandName: "C C Music Facmaxry",
+        musicName: "Gonna Make You Sweat",
+        musicID: "253508531"
+    },
+    11: {
+        bandName: "Cher",
+        musicName: "Believe",
+        musicID: "187003126"
+    },
+    12: {
+        bandName: "Robin S",
+        musicName: "Show Me Love",
+        musicID: "232282747"
+    },
+    13: {
+        bandName: "Culture Beat",
+        musicName: "Mr Vain",
+        musicID: "294660190"
+    },
+    14: {
+        bandName: "Ace of Base",
+        musicName: "All That She Wants",
+        musicID: "225044776"
+    },
+    15: {
+        bandName: "Technotronic",
+        musicName: "Pump Up The Jam",
+        musicID: "252477974"
+    },
+    16: {
+        bandName: "Madonna",
+        musicName: "Vogue",
+        musicID: "214807095"
+    },
+    17: {
+        bandName: "Ace of Base",
+        musicName: "The Sign",
+        musicID: "230451035"
+    },
+    18: {
+        bandName: "Daft Punk",
+        musicName: "Around The World",
+        musicID: "171951429"
+    },
+    19: {
+        bandName: "Montell Jordan",
+        musicName: "This Is How We Do It",
+        musicID: "253186536"
+    }
+};
